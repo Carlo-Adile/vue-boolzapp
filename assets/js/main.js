e37f5dc7375f39420c7c421a950518f526d2aaf0
Carlo-Adile <adile.carlo.a@gmailcom>
2024-03-14 11:52:47 +0100

    building structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
   data() {
-    return{
-      
+    return {
+      myAccount: [
+        {
+          name: "Carlo",
+          bio: "trying to learn Vue Js!",
+          image: './assets/img/avatar_2.jpg'
+        }
+      ]
     }
   }
 })
+
+app.mount('#app')
